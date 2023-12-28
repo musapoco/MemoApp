f287f041b2f385bd2492fd1cefac30b8730df774
musa <153836071+musapoco@users.noreply.github.com>
2023-12-28 12:32:22 +0900

    hhhhh

---
+++ src/app/index.tsx
@@ -0,0 +1,16 @@
+import { View, Text } from 'react-native'
+
+const Index = (): JSX.Element => {
+  return (
+    <View>
+      <view>
+        <view>
+          <text>Memo App</text>
+          <text>ログアウト</text>
+        </view>
+      </view>
+    </View>
+  )
+}
+
+export default Index
